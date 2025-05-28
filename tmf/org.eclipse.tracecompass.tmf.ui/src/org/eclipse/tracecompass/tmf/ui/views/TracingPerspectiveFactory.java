76f5ad3d4c05aa11dd0be584651b494a45523210
Alexandre Arezes <alexarezes16@gmail.com>
2025-05-28 08:45:56 +0200

    fix: open automatically the view for ContractViz +

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/TracingPerspectiveFactory.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/TracingPerspectiveFactory.java
@@ -15,8 +15,8 @@
 package org.eclipse.tracecompass.tmf.ui.views;
 
 import org.eclipse.tracecompass.tmf.ui.project.wizards.NewTmfProjectWizard;
-import org.eclipse.tracecompass.tmf.ui.views.histogram.HistogramView;
-import org.eclipse.tracecompass.tmf.ui.views.statistics.TmfStatisticsView;
+//import org.eclipse.tracecompass.tmf.ui.views.histogram.HistogramView;
+//import org.eclipse.tracecompass.tmf.ui.views.statistics.TmfStatisticsView;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
@@ -37,13 +37,13 @@ public class TracingPerspectiveFactory implements IPerspectiveFactory {
     public static final String ID = "org.eclipse.linuxtools.tmf.ui.perspective"; //$NON-NLS-1$
 
     // Standard TMF views
-    private static final String HISTOGRAM_VIEW_ID = HistogramView.ID;
+//    private static final String HISTOGRAM_VIEW_ID = HistogramView.ID;
 
     // Standard Eclipse views
     private static final String PROJECT_VIEW_ID = IPageLayout.ID_PROJECT_EXPLORER;
-    private static final String STATISTICS_VIEW_ID = TmfStatisticsView.ID;
-    private static final String PROPERTIES_VIEW_ID = IPageLayout.ID_PROP_SHEET;
-    private static final String BOOKMARKS_VIEW_ID = IPageLayout.ID_BOOKMARKS;
+//    private static final String STATISTICS_VIEW_ID = TmfStatisticsView.ID;
+//    private static final String PROPERTIES_VIEW_ID = IPageLayout.ID_PROP_SHEET;
+//    private static final String BOOKMARKS_VIEW_ID = IPageLayout.ID_BOOKMARKS;
 
 
     // ------------------------------------------------------------------------
@@ -56,22 +56,30 @@ public class TracingPerspectiveFactory implements IPerspectiveFactory {
         // Editor area
         layout.setEditorAreaVisible(true);
 
-        // Create the top left folder
         IFolderLayout topLeftFolder = layout.createFolder(
-                "topLeftFolder", IPageLayout.LEFT, 0.15f, IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
+                "topLeftFolder", IPageLayout.LEFT, 0.10f, IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
         topLeftFolder.addView(PROJECT_VIEW_ID);
 
-        // Create the middle right folder
-        IFolderLayout middleRightFolder = layout.createFolder(
-                "middleRightFolder", IPageLayout.BOTTOM, 0.50f, IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
-        middleRightFolder.addView(STATISTICS_VIEW_ID);
 
-        // Create the bottom right folder
+        IFolderLayout right = layout.createFolder(
+                "right", IPageLayout.RIGHT, 0.75f, IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
+        right.addView("org.eclipse.linuxtools.tmf.ui.views.statistics"); //$NON-NLS-1$
+
+        IFolderLayout bright = layout.createFolder(
+                "bright", IPageLayout.BOTTOM, 0.60f, "right"); //$NON-NLS-1$ //$NON-NLS-2$
+        bright.addView("org.example.statediagram.views.statediagramview"); //$NON-NLS-1$
+
+
+        IFolderLayout topRightFolder = layout.createFolder(
+                "topRightFolder", IPageLayout.TOP, 0.95f, IPageLayout.ID_EDITOR_AREA); //$NON-NLS-1$
+        topRightFolder.addView("org.eclipse.tracecompass.analysis.profiling.ui.flamechart:org.eclipse.tracecompass.incubator.traceevent.analysis.callstack"); //$NON-NLS-1$
+
+
         IFolderLayout bottomRightFolder = layout.createFolder(
-                "bottomRightFolder", IPageLayout.BOTTOM, 0.55f, "middleRightFolder"); //$NON-NLS-1$ //$NON-NLS-2$
-        bottomRightFolder.addView(HISTOGRAM_VIEW_ID);
-        bottomRightFolder.addView(PROPERTIES_VIEW_ID);
-        bottomRightFolder.addView(BOOKMARKS_VIEW_ID);
+                "bottomRightFolder", IPageLayout.BOTTOM,0.60f , "topRightFolder"); //$NON-NLS-1$ //$NON-NLS-2$
+
+        bottomRightFolder.addPlaceholder("org.eclipse.linuxtools.internal.tmf.analysis.xml.ui.views.xyview:Ethereum Fee Per Function"); //$NON-NLS-1$
+
 
         // Populate menus, etc
         layout.addPerspectiveShortcut(ID);
